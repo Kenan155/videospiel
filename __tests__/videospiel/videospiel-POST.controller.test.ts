fad815c3a6b9d8f318b10ba7eefd63739a912b14
Anton Schlechter <scan1104@h-ka.de>
2022-11-07 11:56:00 +0100

    Test für POST

---
+++ __tests__/videospiel/videospiel-POST.controller.test.ts
@@ -0,0 +1,198 @@
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { HttpStatus } from '@nestjs/common';
+import { ID_PATTERN } from '../../src/videospiel/service/videospiel-validation.service.js';
+import { MAX_RATING } from '../../src/videospiel/service/jsonSchema.js';
+import { type VideospielDTO } from '../../src/videospiel/rest/videospiel-write.controller.js';
+import { loginRest } from '../login.js';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const neuesVideospiel: VideospielDTO = {
+    titel: 'Testrest',
+    rating: 1,
+    platform: 'Windows',
+    publisher: 'EA',
+    preis: 99.99,
+    rabatt: 0.099,
+    datum: '2022-02-28',
+    speicherplatz: 32.6,
+    homepage: 'https://test.de/',
+    schlagwoerter: ['Shooter', 'Rollenspiel'],
+};
+const neuesVideospielInvalid: Record<string, unknown> = {
+    titel: '!?$',
+    rating: -1,
+    platform: 'UNSICHTBAR',
+    publisher: 'NO_PUBLISHER',
+    preis: 0,
+    rabatt: 2,
+    datum: '12345123123',
+    schlagwoerter: [],
+};
+const neuesVideospielTitelExistiert: VideospielDTO = {
+    titel: 'Alpha',
+    rating: 1,
+    platform: 'Windows',
+    publisher: 'EA',
+    preis: 99.99,
+    rabatt: 0.099,
+    datum: '2022-02-28',
+    speicherplatz: 33.8,
+    homepage: 'https://test.de/',
+    schlagwoerter: ['Shooter', 'Rollenspiel'],
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('POST /', () => {
+    let client: AxiosInstance;
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
+    };
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: (status) => status < 500, // eslint-disable-line @typescript-eslint/no-magic-numbers
+        });
+    });
+
+    // (done?: DoneFn) => Promise<void | undefined | unknown> | void | undefined
+    // close(callback?: (err?: Error) => void): this
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Neues Videospiel', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/',
+            neuesVideospiel,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.CREATED);
+
+        const { location } = response.headers as { location: string };
+
+        expect(location).toBeDefined();
+
+        // ObjectID: Muster von HEX-Ziffern
+        const indexLastSlash: number = location.lastIndexOf('/');
+
+        expect(indexLastSlash).not.toBe(-1);
+
+        const idStr = location.slice(indexLastSlash + 1);
+
+        expect(idStr).toBeDefined();
+        expect(ID_PATTERN.test(idStr)).toBe(true);
+
+        expect(data).toBe('');
+    });
+
+    test('Neues Videospiel mit ungueltigen Daten', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/',
+            neuesVideospielInvalid,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+        expect(data).toEqual(
+            expect.arrayContaining([
+                'Ein Videospieltitel muss mit einem Buchstaben, einer Ziffer oder _ beginnen.',
+                `Eine Bewertung muss zwischen 0 und ${MAX_RATING} liegen.`,
+                'Die Platform eines Videospiels muss Windows,  IOS oder  sein.', //was gibt es noch für Plattformen?
+                'Der publisher eines Videospiels muss EA oder sein.', //hier das selbe
+                'Der Rabatt muss ein Wert zwischen 0 und 1 sein.',
+                'Das Datum muss im Format yyyy-MM-dd sein.',
+                'Der Speicherplatz ist nicht ausreichend.',
+            ]),
+        );
+    });
+
+    test('Neues Videospiel, aber der Titel existiert bereits', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/',
+            neuesVideospielTitelExistiert,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+        expect(data).toEqual(expect.stringContaining('Titel'));
+    });
+
+    test('Neues Videospiel, aber ohne Token', async () => {
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/',
+            neuesVideospiel,
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test('Neues Videospiel, aber mit falschem Token', async () => {
+        // given
+        const token = 'FALSCH';
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/',
+            neuesVideospiel,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.FORBIDDEN);
+        expect(data.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+
+    test.todo('Abgelaufener Token');
+});
