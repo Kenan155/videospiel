903b5049b9c595d2b5666df7a348b215a3d12b52
Pascal Purrucker <pupa1012@h-ka.de>
2022-11-07 18:37:26 +0100

    Create DB

--- extras/postgres/sql/create-db-videospiel.sql
+++ extras/postgres/sql/create-db-videospiel.sql
@@ -17,13 +17,13 @@
 --     d.h. auskommentieren in docker-compose.yaml
 -- (2) docker compose exec postgres bash
 -- (3) chown postgres:postgres /var/lib/postgresql/tablespace
--- (4) chown postgres:postgres /var/lib/postgresql/tablespace/buch
+-- (4) chown postgres:postgres /var/lib/postgresql/tablespace/videospiel
 -- (5) exit
 -- (6) docker compose down
 -- (7) in docker-compose.yaml den User "postgres" wieder aktivieren
 -- (8) docker compose up
 -- (9) docker compose exec postgres bash
--- (10) psql --dbname=postgres --username=postgres --file=/sql/create-db-buch.sql
+-- (10) psql --dbname=postgres --username=postgres --file=/sql/create-db-videospiel.sql
 -- (11) exit
 
 -- https://www.postgresql.org/docs/current/sql-createrole.html
